fd86e93132603968e340d778d885f5b0129cad2b
Junaidejaz111 <45161344+Junaidejaz111@users.noreply.github.com>
2022-04-28 15:49:20 +0500

    changed nav positions

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,9 +14,9 @@ function Navbar() {
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active"> <a className="nav-link" href="#home">Home</a> </li>
         <li className="nav-item"> <a className="nav-link" href="#about">About</a> </li>
+        <li className="nav-item"> <a className="nav-link" href="#work">Portfolio</a> </li>
         
         <li className="nav-item"> <a className="nav-link" href="#client">Testimonial</a> </li>
-        <li className="nav-item"> <a className="nav-link" href="#work">Portfolio</a> </li>
 
         <li className="nav-item"> <a className="nav-link" href="#contact">Contact</a> </li>
       </ul>
